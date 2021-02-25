0b3ac98c36e1a2301952fb527f94a0df9f438067
Lydia Smith <l8smith@ucsd.edu>
2021-02-25 12:38:17 -0600

    addd 3rd party api

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response) {
+var rsvpEmail = request.body.rsvpEmail;
+	//print to console the PST request made
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail); //add to current data
+
+	//send back data to know it worked
+	response.send(rsvpEmail);
+};
